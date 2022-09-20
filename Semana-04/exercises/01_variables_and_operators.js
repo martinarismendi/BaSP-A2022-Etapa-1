a61dbc1299f66ffa7de89a795e441a956d37afeb
Martin Arismendi <martinarismendi96@gmail.com>
2022-09-20 20:51:15 -0300

    BaSP-W04-2022:Finish exercise 1

---
+++ Semana-04/exercises/01_variables_and_operators.js
@@ -0,0 +1,21 @@
+//Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+
+var num1 = 5;
+var num2 = 3;
+var result = num1 + num2;
+console.log(result);
+
+//Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+var str1 = "Hello word,";
+var str2 = "whats up?";
+var str3 = str1 + str2;
+console.log(str3);
+
+/*Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) 
+  guardando el resultado de la suma en una 3er variable (utilizar length)*/
+
+var str1lengh = "Hello word, ";
+var str2lengh = "how are you?";
+var str3lengh = str1lengh.length + str2lengh.length;
+console.log(str3lengh);
